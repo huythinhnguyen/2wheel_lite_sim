9c86249bebbc6b958bae5e95833173054a6d9b86
thinh <huythinhnguyen@gmail.com>
2022-07-06 23:47:02 -0400

    ran a sample test. Need to check over the data. Issue might be in the controller but can also be in some unit conversion

--- sample_run.py
+++ sample_run.py
@@ -10,12 +10,13 @@ desired_kinematics = np.array([]).reshape(0,2)
 kinematics = np.array([]).reshape(0,2)
 state = State()
 drive = Drive()
-c = NaiveSquare(init_pose = state.pose, width=1)
+c = NaiveSquare(init_pose = state.pose, width=5)
 i = 0
+print('Step\tpose\t\t\tpose_hat\t\t\tkinematic\t\tkinenatic_hat\t')
 while not c._run_ended:
     pose = state.pose
     pose_hat = c.gps.pose_hat
-    poses = np.vstack((pose, pose))
+    poses = np.vstack((poses, pose))
     poses_hat = np.vstack((pose_hat, poses_hat))
     desired_kinematic = c.track_goal(pose)
     drive.kinematic_update(new_kinematic=desired_kinematic)
@@ -24,19 +25,28 @@ while not c._run_ended:
     desired_kinematics = np.vstack((desired_kinematics, desired_kinematic))
     kinematics = np.vstack((kinematics, drive.kinematic))
     i+=1
-    print('Step\tpose\t\t\tpose_hat\t\t\tkinematic\t\tkinenatic_hat\t')
-    print(i,'\t',np.round(pose,3),'\t\t\t',np.round(pose_hat,3),'\t\t\t',np.round(desired_kinematic,3),'\t\t',np.round(drive.kinematic,3))
+    print(i,end='\t')
+    print(np.round(pose,3),end='\t')
+    print(np.round(pose_hat,3),end='\t')
+    print(np.round(desired_kinematic,3),end='\t')
+    print(np.round(drive.kinematic,3))
+    if i > 10_000: break
     
 print('DONE. SAVE DATA')    
 np.savez('data.npz', poses=poses, poses_hat=poses_hat, desired_kinematics=desired_kinematics, kinematics=kinematics)
 
+print(poses.shape)
+print(poses_hat.shape)
+
 from matplotlib import pyplot as plt
 fig, (ax1, ax2, ax3) = plt.subplots(3,1)
-ax1.scatter(pose[:,0], pose[:,1], s=5, label='pose')
-ax1.scatter(pose_hat[:,0], pose[:,1],s=5, label='pose_hat')
+ax1.scatter(poses[:,0], poses[:,1], s=1, label='pose', alpha=0.3)
+ax1.scatter(poses_hat[:,0], poses_hat[:,1],s=1, label='pose_hat', alpha=0.3)
 ax1.legend()
-ax2.plot(desired_kinematics[:,0], 'desired v')
-ax2.plot(kinematics[:,0], 'actual v')
-ax3.plot(desired_kinematics[:,1], 'desired w')
-ax3.plot(kinematics[:,1], 'actual w')
+ax2.plot(desired_kinematics[:,0], label='desired v', alpha=0.5)
+ax2.plot(kinematics[:,0], label='actual v', alpha=0.5)
+ax2.legend()
+ax3.plot(desired_kinematics[:,1], label='desired w', alpha=0.5)
+ax3.plot(kinematics[:,1], label='actual w', alpha=0.5)
+ax3.legend()
 plt.show()
