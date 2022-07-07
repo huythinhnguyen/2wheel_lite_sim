e24f3d386dd18d3f7440931a1968f20355ad1038
thinh <huythinhnguyen@gmail.com>
2022-07-06 23:29:52 -0400

    All modules in Simulation ran! Still need to check for running bug and build Animation

--- Simulation/Motion.py
+++ Simulation/Motion.py
@@ -37,7 +37,7 @@ class State:
 
     
     def turning_radius(self):
-        if np.abs(self.w) < 0.001: return self.v/self.w
+        if np.abs(self.kinematic[1]) < 0.001: return self.kinematic[0]/self.kinematic[1]
         else: return 'inf'
 
 
