12ee1a43f46fb0a207433a0e721d5beb1a0829d9
thinh <huythinhnguyen@gmail.com>
2022-07-26 16:17:30 -0400

    ..

--- Animation/ObjUtils.py
+++ Animation/ObjUtils.py
@@ -6,12 +6,12 @@ from matplotlib import pyplot as plot
 
 class Agent:
     def __init__(self):
-        self.arrow_color = 'tab:brow'
+        self.arrow_color = 'tab:red'
         self.dot_color = 'tab:orange'
         self.arrow_length = 1.0
-        self.arrow_aspect = 0.01
-        self.arrow_width = self.arrow_length * self.aspect
-        self.arrow_alpha = 1.0
+        self.arrow_aspect = 0.005
+        self.arrow_width = self.arrow_length * self.arrow_aspect
+        self.arrow_alpha = 0.8
         self.course_color = 'tab:blue'
         self.course_size = 0.2
         self.course_alpha = 0.5
@@ -22,12 +22,12 @@ class Agent:
             poses.reshape(-1,3)
     
         ax.quiver(poses[:,0], poses[:,1], self.arrow_length*np.cos(poses[:,2]), self.arrow_length*np.sin(poses[:,2]),
-                  pivot='tail', scale_units='xy', scale=1.0, width = self.arrow_width, color=self.arrow_collor, alpha=self.arrow_alpha)
+                  pivot='tail', scale_units='xy', scale=1.0, width = self.arrow_width, color=self.arrow_color, alpha=self.arrow_alpha)
         ax.set_aspect('equal')
         xr = ax.get_xlim()[1] - ax.get_xlim()[0] + 1
         yr = ax.get_ylim()[1] - ax.get_ylim()[0] + 1
         scatter_scale = ax.get_window_extent().width/(max(xr,yr)**2)
-        ax.scatter(poses[:,0], poses[:,1], s=20*scale, alpha=self.arrow_alpha)
+        ax.scatter(poses[:,0], poses[:,1], s=20*scatter_scale, alpha=self.arrow_alpha)
         return ax
 
 
@@ -51,4 +51,4 @@ class Food:
 
 class Plant:
     def __init__(self):
-        pass
+        
