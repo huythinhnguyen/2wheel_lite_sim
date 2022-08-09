028ed906d8c2ba25414243daf1bc58237d87ec4e
thinh <huythinhnguyen@gmail.com>
2022-08-09 11:50:01 -0400

    add get path for data file in Spatializer

--- Sensors/BatEcho/Spatializer.py
+++ Sensors/BatEcho/Spatializer.py
@@ -5,11 +5,13 @@ Spatializer reference sounds into scence when given locations of sound sources
 import numpy as np
 from . import Cochlear
 import sys
-
+import os
 
 SAMPLE_FREQ = 3e5
 SPEED_OF_SOUND = 340
-DISTANCE_ENCODING = np.arange(1,7001) * 0.5 (1/sample_freq) * SPEEED_OF_SOUND
+DISTANCE_ENCODING = np.arange(1,7001) * 0.5 * (1/SAMPLE_FREQ) * SPEED_OF_SOUND
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/dataset")
 
 class Retriever:
     def __init__(self, ):
@@ -26,26 +28,39 @@ class Retriever:
         self.non_random_index = None
         self.angle_step = 1
         self.raw_length = 7000
-        self.objects_dict = {'bg': 0, 'pole':1, 'plant':2}
+        self.objects_dict = {'background': 0, 'pole':1, 'plant':2}
+        for i in range(-1,-len(sys.path), -1):
+            if 'dataset' in sys.path[i]:
+                self.DATAROOT = sys.path[i]
+                break
 
-
+            
     def _get_reference(self,objects):
         if not self.random:
             np.random.seed(1)
-            if self.non_random_index is not None: selection = self.non_random_index
-            else: raise ValueError('must input index when random=False')
+            if self.non_random_index is not none: selection = self.non_random_index
+            else: raise valueerror('must input index when random=false')
         distances = [objects[0]] if type(objects)==list else list(objects[:,0])
         angles = [objects[1]] if type(objects)==list else list(objects[:,1])
         klasses = [objects[2]] if type(objects)==list else list(objects[:,2])
         left_echoes, right_echoes = np.empty((len(distances),self.raw_length)), np.empty((len(distances),self.raw_length))
         for i, (distance, angle, klass) in zip(distances, angles, klasses):
             path = self._get_data_path(distance, angle, klass)
-            left_set = np.load(path + 'left.npy')
-            right_set = np.load(path + 'right.npy')
+            left_set = np.load(os.path.join(path, 'left.npy'))
+            right_set = np.load(os.path.join(path, 'right.npy'))
             if self.random: selection = np.random.randint(len(left_set))
             left_echoes[i], right_echoes[i] = left_set[selection,:], right_set[selection,:]
         return left_echoes, right_echoes
 
+
+    def _get_data_path(self,distance,angle,klass):
+        d = str(np.round(float(distance),2))
+        a = str(np.round(float(angle) ))
+        k = list(self.objects_dict.keys())[list(self.objects_dict.values()).index(int(klass))]
+        path = os.path.join(self.DATAROOT,k,d+'_'+a)
+        return path
+        
+        
 
     def _get_angle_interpolated_reference(self,objects): # LINEAR
         if type(objects)==list: objects = np.array(objects).reshape(-1,3)
@@ -112,7 +127,7 @@ class Render:
 
 _POLE_STARTS  = {0.25: 0.13, 0.5: 0.38, 0.75: 0.62, 1.0: 0.88, 1.25: 1.12, 1.5: 1.36, 1.75: 1.62, 2.0: 1.87, 2.25: 2.11, 2.5: 2.35}
 _POLE_ENDS    = {0.25: 0.47, 0.5: 0.66, 0.75: 0.88, 1.0: 1.1, 1.25: 1.31, 1.5: 1.53, 1.75: 1.76, 2.0: 1.98, 2.25: 2.22, 2.5: 2.48}
-_POLREV_STARTS= {0.25: 0.89, 0.5: 0.79, 0.75: 0.94,1.0: 1.13, 1.25: 1.33, 1.5: 1.55, 1.75: 1.78, 2.0: 2.01, 2.25: 2.24}
-_POLREV_ENDS  = {0.25: 1.13, 0.5: 0.99, 0.75: 1.15, 1.0: 1.33, 1.25: 1.51, 1.5: 1.71, 1.75: 1.92, 2.0: 2.14, 2.25: 2.35}
+_POLEREV_STARTS= {0.25: 0.89, 0.5: 0.79, 0.75: 0.94,1.0: 1.13, 1.25: 1.33, 1.5: 1.55, 1.75: 1.78, 2.0: 2.01, 2.25: 2.24}
+_POLEREV_ENDS  = {0.25: 1.13, 0.5: 0.99, 0.75: 1.15, 1.0: 1.33, 1.25: 1.51, 1.5: 1.71, 1.75: 1.92, 2.0: 2.14, 2.25: 2.35}
 _PLANT_STARTS = {0.5: 0.26, 0.75: 0.49, 1.0: 0.76, 1.25: 0.99, 1.5: 1.24, 1.75: 1.49, 2.0: 1.74, 2.25: 1.91, 2.5: 2.24}
 _PLANT_ENDS   = {0.5: 1.29, 0.75: 1.35, 1.0: 1.57, 1.25: 1.71, 1.5: 1.76, 1.75: 1.88, 2.0: 2.38, 2.25: 2.47, 2.5: 2.72}
