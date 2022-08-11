09e3e700afd6b85cf5896e5f1b4f2514784b3b79
thinh <huythinhnguyen@gmail.com>
2022-08-11 12:06:54 -0400

    speed up the Cochlear code

--- Sensors/BatEcho/Cochlear.py
+++ Sensors/BatEcho/Cochlear.py
@@ -14,22 +14,34 @@ class CochlearFilter:
         self.gammatone_banksize = 1
         self.exp_compression_power = 0.4
         self.lowpass_freq = 1e3
-
+        self.cache = {}
 
     def transform(self, data):
         # Broad bandpass filter
-        b, a = butter(self.broadband_spec['order'],
-                      [self.broadband_spec['low'],self.broadband_spec['high']],
-                      fs = self.sampling_freq )
-        y = lfiter(b,a,data)
+        if 'broadband' not in self.cache.keys():
+            b, a = butter(self.broadband_spec['order'],[self.broadband_spec['low'],self.broadband_spec['high']],
+                          'band', fs = self.sampling_freq )
+            self.cache['broadband'] = (b, a)
+        else: b, a = self.cache['broadband']
+        y = lfilter(b,a,data)
         # Gammatone Filter
-        b, a = gammatone(self.emission_freq, 'fir', fs=self.sampling_freq)
+        if 'gammatone' not in self.cache.keys():
+            b, a = gammatone(self.emission_freq, 'fir', fs=self.sampling_freq)
+            self.cache['gammatone'] = (b, a)
+        else: b, a = self.cache['gammatone']
         y = lfilter(b,a,y)
         # halfwave rectifier
         y[y<0] = 0
         # exponential compression
         y = np.power(y,0.4)
         # lowpass filter
-        b,a = butter(2, self.lowpass_freq, 'low', fs=self.sampling_freq)
+        if 'lowpass' not in self.cache.keys():
+            b,a = butter(2, self.lowpass_freq, 'low', fs=self.sampling_freq)
+            self.cache['lowpass'] = (b,a)
+        else: b, a = self.cache['lowpass']
         y = lfilter(b, a, y)
         return y
+
+
+    def _reset(self):
+        self.cache.clear()
