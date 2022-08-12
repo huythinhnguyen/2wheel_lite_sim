76f9705b84aafef8ef9cc6b4edf6a8785b1bb5e0
thinh <huythinhnguyen@gmail.com>
2022-08-12 16:20:00 -0400

    working on BatECho.py.......

--- Control/SensorimotorLoops/BatEcho.py
+++ Control/SensorimotorLoops/BatEcho.py
@@ -1,9 +1,15 @@
 import numpy as np
+import sys
+import os
 
+sys.path.append(os.path.dirname(os.path.abspath(__path__)) + '/./Sensors/BatEcho/')
 
 class Avoid:
-    def __init__(self):
-        pass
+    def __init__(self, DISTANCE_REFERENCE=None):
+        if DISTANCE_REFERENCE is None:
+            raise ValueError('pass Compressors.Subsample.DISTANCE_REFERENCE to DISTANCE_REFERENCE')
+        self.DISTANCE_REFERENCE = DISTANCE_REFERENCE
+        self.window_size=10
 
 
 class Approach:
