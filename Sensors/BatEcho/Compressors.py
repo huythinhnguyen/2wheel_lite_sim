3788df6e51e46647a3ae690fc0b5d592996f4a10
thinh <huythinhnguyen@gmail.com>
2022-08-16 13:16:54 -0400

    update Setting

--- Sensors/BatEcho/Compressors.py
+++ Sensors/BatEcho/Compressors.py
@@ -8,18 +8,10 @@ class Subsample:
         self.quiet_threshold = quiet_threshold
         self.quiet = True
         self.truncate = truncate
-        self.DISTANCE_REFERENCE = np.mean(Setting.DISTANCE_ENCODING.reshape(-1,self.n_sample),
-                                          axis=1)[:int(self.n_sample-self.truncate)]
-
+        
 
     def transform(self, data):
         if type(data) != np.ndarray: data=np.asarray(data)
         data = np.mean(data.reshape(-1,self.n_sample), axis=1)
         if self.quiet: data[data<self.quiet_threshold] = 0.
         return data[:int(self.n_sample-self.truncate)]
-
-
-    def _reset(self, n_sample=None):
-        if n_sample is not None: self.n_sample=n_sample
-        self.DISTANCE_REFERENCE = np.mean(DISTANCE_ENCODING.reshape(-1,self.n_sample),
-                                          axis=1)[:int(self.n_sample-self.truncate)]
