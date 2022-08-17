16630a7c9bb622dd605578714200927bf6fc4215
thinh <huythinhnguyen@gmail.com>
2022-08-17 13:34:42 -0400

    small fix

--- Control/SensorimotorLoops/BatEcho.py
+++ Control/SensorimotorLoops/BatEcho.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__path__)) + '/./Sensors/BatEcho/')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../')
 
 from Sensors.BatEcho import Setting, Spatializer
 from . import Setting as config
