e2a79617260872f59b6ac64d4862a21d48bf82fe
thinh <huythinhnguyen@gmail.com>
2022-08-22 11:12:48 -0400

    tested all the tunnels

--- TestRun/AvoidanceLoopTest_08.21.22.py
+++ TestRun/AvoidanceLoopTest_08.21.22.py
@@ -1,9 +1,11 @@
 import numpy as np
 import os
+import sys
 
 os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../'))
-print('Current working path:\n', os.getcwd())
+sys.path.append(os.getcwd())
 
+from Arena import Builder
 
 """
 different types of maze:
@@ -19,7 +21,6 @@ different types of maze:
 """
 class Maze:
     def __init__(self, maze_id, obstacles_type=['plant'], **kwargs):
-        from Arena import Builder
         self.maze_id = maze_id
         if obstacles_type[0]=='plant': self.spacing = 0.3
         if obstacles_type[0]=='pole' : self.spacing = 0.1
@@ -51,13 +52,14 @@ class Maze:
             e2u, e2d = [3*self.maze_size/4 - self.tunnel_width/2, 0],[3*self.maze_size/4 + self.tunnel_width/2, 0]
             s3u, s3d = e2u.copy(), e2d.copy()
             e3u, e3d = [self.maze_size, self.maze_size/4 + self.tunnel_width/2],[self.maze_size, self.maze_size/4 - self.tunnel_width/2]
-            arc1u = Builder.build_arc(s1u,e1u, self.maze_size/4 + self.tunnel_width/2, self.spacing, spin=-1)
-            arc1d = Builder.build_arc(s1d,e1d, self.maze_size/4 - self.tunnel_width/2, self.spacing, spin=-1)
+            
+            arc1u = Builder.build_arc(s1u,e1u, radius=self.maze_size/4 + self.tunnel_width/2, spacing=self.spacing, spin=-1)
+            arc1d = Builder.build_arc(s1d,e1d, radius=self.maze_size/4 - self.tunnel_width/2, spacing=self.spacing, spin=-1)
             arc2u = Builder.build_arc(s2u,e2u, self.maze_size/4 - self.tunnel_width/2, self.spacing, spin= 1)
             arc2d = Builder.build_arc(s2d,e2d, self.maze_size/4 + self.tunnel_width/2, self.spacing, spin= 1)
             arc3u = Builder.build_arc(s3u,e3u, self.maze_size/4 + self.tunnel_width/2, self.spacing, spin=-1)
             arc3d = Builder.build_arc(s3d,e3d, self.maze_size/4 - self.tunnel_width/2, self.spacing, spin=-1)
-            temp = np.vstack((arc1u, arc1d, arc2u, ar2d, arc3u, arc3d))
+            temp = np.vstack((arc1u, arc1d, arc2u, arc2d, arc3u, arc3d))
             temp[:,0] = temp[:,0] + i * self.maze_size
             arcs = np.vstack((arcs,temp))
         return arcs
@@ -95,11 +97,11 @@ class Maze:
             primer[0] = primer[0] + np.cos(primer[2])*self.maze_size/2
             primer[1] = primer[1] + np.sin(primer[2])*self.maze_size/2
             primer[2] = Builder.wrap2pi(primer[2] - (np.pi - self.zigzag_angle))
-            ends.append([primer[0]+d*np.cos(Builder.wrap2pi(primer[2] + self.zigzag_angle/2)),
-                         primer[1]+d*np.sin(Builder.wrap2pi(primer[2] + self.zigzag_angle/2))])
+            ends.append([primer[0]+d*np.cos(Builder.wrap2pi(primer[2] + (np.pi-self.zigzag_angle/2) )),
+                         primer[1]+d*np.sin(Builder.wrap2pi(primer[2] + (np.pi-self.zigzag_angle/2) ))])
             starts.append(ends[-1])
-            ends.append([primer[0]+d*np.cos(Builder.wrap2pi(primer[2]-(np.pi-self.zigzag_angle/2))),
-                         primer[1]+d*np.sin(Builder.wrap2pi(primer[2]-(np.pi-self.zigzag_angle/2)))])
+            ends.append([primer[0]+d*np.cos(Builder.wrap2pi(primer[2]-(self.zigzag_angle/2))),
+                         primer[1]+d*np.sin(Builder.wrap2pi(primer[2]-(self.zigzag_angle/2)))])
             starts.append(ends[-1])
             if i == self.cycle_number - 1:
                 primer[0] = primer[0] + np.cos(primer[2])*self.maze_size/2
@@ -112,7 +114,7 @@ class Maze:
                          primer[1] + (self.tunnel_width/2)*np.sin(primer[2] + np.pi/2)])
             ends.append([primer[0] + (self.tunnel_width/2)*np.cos(primer[2] - np.pi/2),
                          primer[1] + (self.tunnel_width/2)*np.sin(primer[2] - np.pi/2)])            
-        spacings = np.ones(len(starts))*self.spacings
+        spacings = np.ones(len(starts))*self.spacing
         
         return Builder.build_walls(starts, ends, spacings)
 
@@ -125,8 +127,15 @@ class Recorder:
         self.cache = {}
 
 
-
 if __name__=='__main__':
-    mazeBuilder = Maze(maze_id='1.a', maze_size=8., tunnel_width=3.)
-    maze = mazeBuilder._straigth_tunnel()
+    from matplotlib import pyplot as plt
+    from Animation import ObjUtils, Render
+  
+    mazeBuilder = Maze(maze_id='1.a', maze_size=10., tunnel_width=2., cycle_number=3, zigzag_angle=np.pi/2)
+    maze = mazeBuilder._zigzag_tunnel()
+
+    plant = ObjUtils.Plant()
+    fig, ax = plt.subplots()
+    plant.plot(ax, maze)
+    plt.show()
     
