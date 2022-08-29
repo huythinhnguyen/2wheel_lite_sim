388f401b2a14950b48f8637b18e579d5537240b3
thinh <huythinhnguyen@gmail.com>
2022-08-29 16:01:57 -0400

    update using Log and dB for gain

--- Sensors/BatEcho/GainCurve.py
+++ Sensors/BatEcho/GainCurve.py
@@ -1,4 +1,3 @@
-from re import L
 import numpy as np
 from . import Setting
 
@@ -10,14 +9,18 @@ class EarGain:
         self.rose_k = Setting.NUMBER_OF_PEDALS if 'rose_k' not in kwargs.keys() else kwargs['rose_k']
         self.rose_b = Setting.ROSE_CURVE_B if 'rose_b' not in kwargs.keys() else kwargs['rose_b']
         self.rose_a = 1 - self.rose_b
-        
+        self.min_gain = Setting.MIN_GAIN_DB
+    
 
     def get_gain_ratio(self, target_angle, ref_angle, radians=True):
-        target_gain = self._normalized_gain(target_angle, radians=radians)
-        ref_gain = self._normalized_gain(ref_angle, radians=radians)
-        return np.divide(target_angle, ref_angle)
-    
-    
+        gain_diff = self._normalized_gain(target_angle,radians=radians)-self._normalized_gain(ref_angle,radians=radians)
+        return np.power(10, gain_diff/20)
+
+
     def _normalized_gain(self, angle, radians=True):
         theta = np.copy(angle) if radians else np.radians(angle)
-        return self.rose_a  + self.rose_b*np.cos(self.rose_k*theta - 2*self.ear_angle)
+        temp=self.rose_a  + self.rose_b*np.cos(self.rose_k*theta - self.ear_angle)
+        if temp<np.power(10,self.min_gain/20): temp = np.power(10,self.min_gain/20)
+        gain = 20*np.log10(temp)
+        return gain
+
