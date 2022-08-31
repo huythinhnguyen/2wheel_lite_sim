7a4a2a13ee9a6b14be1189ad451d2ca9215fb5e5
thinh <huythinhnguyen@gmail.com>
2022-08-31 12:28:23 -0400

    update Setting

--- Control/SensorimotorLoops/Setting.py
+++ Control/SensorimotorLoops/Setting.py
@@ -1,15 +1,24 @@
 from math import pi
 
+GRAVI_ACCEL = 9.8
+
 ###################################################################
 ### VELOCITY LIMIT  ###############################################
 ###################################################################
 MAX_LINEAR_VELOCITY = 5 # robot max velo is 0.5 m/s
-MAX_ANGULAR_VELOCITY = 20*pi
-MAX_ANGULAR_ACCELERATION = float('inf') #0.5*MAX_ANGULAR_VELOCITY
+MAX_ANGULAR_VELOCITY = 10*pi
+MAX_ANGULAR_ACCELERATION = 1*MAX_ANGULAR_VELOCITY
 LINEAR_VELOCITY_OFFSET = 0.
 DECELERATION_FACTOR = 1 # Choose between 1 to 5 the higher the steeper the deceleration
-CENTRIFUGAL_ACCEL = 3
+CENTRIFUGAL_ACCEL = 2 * GRAVI_ACCEL
 CHIRP_RATE = 40 # Hz
+
+### OTHERS KINEMATIC PARAMETERS  ##################################
+TAU_K = 0.1
+LINEAR_DECEL_LIMIT = -1.1 * MAX_LINEAR_VELOCITY
+LINEAR_ACCEL_LIMIT = 1 * GRAVI_ACCEL
+BODY_RADIUS = 0.15
+BAIL_DISTANCE_MULTIPLIER = 5
 
 ###################################################################
 ###  ROBOT CONVERSION   ###########################################
