11f1895eed28d5f3e9016c869f9616280d6e20b1
thinh <huythinhnguyen@gmail.com>
2022-09-09 12:19:42 -0400

    add maze builder

--- Gym/BatSnake_v0/Helper.py
+++ Gym/BatSnake_v0/Helper.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 HIT_DISTANCE = {1: 0.3, 2: 0.4}
+MAZE_SIZE = 16.
+TUNNEL_SIZE = 3.
 
 
 def collision_check(inview, object_class):
-    inview_of_klass = inview[inview[:,2]==object_class][:, :2]
-    if np.sum(inview[:,0]<HIT_DISTANCE[object_class]) > 0:
+    inview_of_klass = inview[inview[:,2]==object_class][:,:2]
+    if np.sum(inview_of_klass[:,0]<HIT_DISTANCE[object_class]) > 0:
         return True
     else:
         return False
@@ -17,3 +19,8 @@ def reward_function(**kwargs):
         if hit == 'plant': return -1
         if hit == 'pole': return +1
     else: return 0
+
+
+def maze_builder(mode, maze_size=MAZE_SIZE, tunnel_size=TUNNEL_SIZE):
+    
+    return None
