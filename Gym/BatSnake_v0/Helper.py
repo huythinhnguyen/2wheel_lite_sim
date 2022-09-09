e130ec5832e84aef0bf31195d374d24cf046f8df
thinh <huythinhnguyen@gmail.com>
2022-09-09 10:18:12 -0400

    add collision_check

--- Gym/BatSnake_v0/Helper.py
+++ Gym/BatSnake_v0/Helper.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+HIT_DISTANCE = {1: 0.3, 2: 0.4}
+
+
+def collision_check(inview, object_class):
+    inview_of_klass = inview[inview[:,2]==object_class][:, :2]
+    if np.sum(inview[:,0]<HIT_DISTANCE[object_class]) > 0:
+        return True
+    else:
+        return False
