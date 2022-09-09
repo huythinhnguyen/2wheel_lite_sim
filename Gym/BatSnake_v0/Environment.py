01c3b5f589a99a681d8878a97cf17982e0885653
thinh <huythinhnguyen@gmail.com>
2022-09-09 09:54:34 -0400

    ..

--- Gym/BatSnake_v0/Environment.py
+++ Gym/BatSnake_v0/Environment.py
@@ -20,12 +20,39 @@ from Simulation.Motion import State
 
 class DiscreteAction(py_environment.PyEnvironment):
     def __init__(self, init_pose, time_limit):
-        self.locomotion = State(pose = init_pose)
-        self.sonar = Spatializer()
+        self.locomotion = State(pose = init_pose, dt=1/controlconfig.CHIRP_RATE)
+        self.sensor = Spatializer()
         self.controller = AvoidApproach()
 
         self._action_spec=array_spec.BoundedArraySpec(shape=(), dtype=np.int32, minimum=0, maximum=1, name='action')
         self._observation_spec=array_spec.BoundedArraySpec(shape=(100,), dtype=np.float64, minimum=0, name='observation')
-        self._state = None # ECHO INPUT
+        self._state = np.zeros(100) # ECHO INPUT
         self._episode_ended = False
         self.time_limit = time_limit
+        self.cache = {'init_pose': init_pose}
+
+    def action_spec(self):
+        return self._action_spec
+
+    def observation_spec(self):
+        return self._observation_spec
+
+    
+    def _step(self, action, **kwargs):
+        reward = 0
+        if self._episode_ended:
+            return self._reset()
+        
+        # Do things here!
+
+    
+    def _reset(self, **kwargs):
+        self._episode_ended=False
+        init_pose = kwargs['init_pose'] if 'init_pose' in kwargs.keys() else self.cache['init_pose']
+        self.locomotion = State(pose=init_pose, dt=1/controlconfig.CHIRP_RATE)
+        self.sensor = Spatializer()
+        self.controller = AvoidApproach()
+
+        return ts.restart(np.array([self._state], dtype=np.float64).reshape(len(self._state),))
+
+    
