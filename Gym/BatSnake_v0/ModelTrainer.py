a8700a6c5b23f101a0b0123a3d75f75a4311d08f
thinh <huythinhnguyen@gmail.com>
2022-09-19 12:54:09 -0400

    ..

--- Gym/BatSnake_v0/ModelTrainer.py
+++ Gym/BatSnake_v0/ModelTrainer.py
@@ -1,7 +1,9 @@
+from email import policy
 import os
 import sys
 import pathlib
 
+from . import Helper as help
 if pathlib.Path(os.path.abspath(__file__)).parents[2] not in sys.path:
     sys.path.append(str(pathlib.Path(os.path.abspath(__file__)).parents[2]))
 
@@ -11,7 +13,7 @@ from Gym.BatSnake_v0.Environment import DiscreteAction
 
 import numpy as np
 import tensorflow as tf
-tf.autograph.set_verbosity(2)
+tf.autograph.set_verbosity(0)
 
 from tf_agents.agents.dqn import dqn_agent
 from tf_agents.environments import tf_py_environment
@@ -27,10 +29,9 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.initializers import RandomUniform, Constant, VarianceScaling
 from tensorflow.keras.activations import relu, linear
 
-
 HIDDEN_LAYER_PARAMS = (128, 128, 128, 64)
 
-TRAINING_STEPS = 2_000_000
+TRAINING_STEPS = 3_000_0
 INITIAL_COLLECTION_STEPS = 1_000
 COLLECT_STEPS_PER_ITERATION = 1
 REPLAY_BUFFER_MAX_LENGTH = 500_000
@@ -38,10 +39,10 @@ REPLAY_BUFFER_MAX_LENGTH = 500_000
 PARALLEL_CALLS = 32
 BATCH_SIZE = 1024
 LEARNING_RATE = 3e-4
-LOG_STEPS_INTERVAL = 20_000
+LOG_STEPS_INTERVAL = 20_0
 NUMBER_OF_EVAL_EPISODES = 10
-EVAL_STEPS_INTERVAL = 20_000
-POLICY_SAVER_INTERVAL = 500_000
+EVAL_STEPS_INTERVAL = 20_0
+POLICY_SAVER_INTERVAL = 500_0
 
 STARTING_EPSILON = 0.8
 EPSILON_DECAY_COUNT = 2_000_000
@@ -50,12 +51,13 @@ DISCOUNT_FACTOR = 0.999
 TD_ERROR_LOSS_FUNCTION = common.element_wise_squared_loss
 TRAIN_STEP_COUNTER = 0
 
-DATE = '09.15.22'
+DATE = '09.19.22'
 NOTES =''
 CHECKPOINT_DIRECTORY = 'TrainedAgents'
-TIME_LIMIT = 250
+TIME_LIMIT = 500
+DIFFICULTY = 1
 
-INIT_POLICY = None # Placed Previously Trained Policy Here.
+INIT_POLICY = None # help.load_policy(checkpoint_dir=CHECKPOINT_DIRECTORY, agent_id='09.15.22') # Placed Previously Trained Policy Here.
 
 ### Build some Function building model here!
 ### Build some convenience saver if needed. :D
@@ -120,13 +122,13 @@ def compute_average_return(environment, policy, number_of_episodes, getcache=Fal
 
 def train_v1(init_policy=None):
     phase = 0
-    py_env = DiscreteAction(time_limit = TIME_LIMIT, phase=phase, log=True)
+    py_env = DiscreteAction(time_limit = TIME_LIMIT, difficulty=DIFFICULTY phase=phase, log=True)
     tf_env = tf_py_environment.TFPyEnvironment(py_env)
     #action_tensor_spec = tensor_spec.from_spec(py_env.action_spec())
     num_actions = 2 #action_tensor_spec.maximum - action_tensor_spec.minimum + 1
     q_net = q_network(hidden_layer_params=HIDDEN_LAYER_PARAMS, number_of_actions=num_actions)
     
-    eval_py_env = DiscreteAction(time_limit = TIME_LIMIT, phase=phase, log=True)
+    eval_py_env = DiscreteAction(time_limit = TIME_LIMIT, difficulty=DIFFICULTY phase=phase, log=True)
     eval_tf_env = tf_py_environment.TFPyEnvironment(eval_py_env)
 
     agent = summon_agent(tf_env, q_net)
@@ -166,7 +168,17 @@ def train_v1(init_policy=None):
     else: save_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), CHECKPOINT_DIRECTORY+'/'+DATE+NOTES)
     print('Policy will be save to:\n', save_dir)
     policy_dir = os.path.join(save_dir, 'policy')
+    checkpoint_dir = os.path.join(save_dir,'checkpoint')
+
+    if not os.path.exists(checkpoint_dir): os.makedirs(checkpoint_dir)
     if not os.path.exists(policy_dir): os.makedirs(policy_dir)
+    train_checkpointer = common.Checkpointer(ckpt_dir=checkpoint_dir,
+                                             max_to_keep=10,
+                                             agent=agent,
+                                             policy=agent.policy
+                                             replay_buffer=replay_buffer,
+                                             global_step=agent.train_step_counter)
+                                             
     tf_policy_saver = policy_saver.PolicySaver(eval_policy)
     
 
@@ -181,7 +193,9 @@ def train_v1(init_policy=None):
             losses.append(train_loss)
         if step % EVAL_STEPS_INTERVAL == 0:
             # save policy:
-            if step%POLICY_SAVER_INTERVAL==0: tf_policy_saver.save( os.path.join(policy_dir,str(int(step/1e6))+'M_steps'))
+            if step%POLICY_SAVER_INTERVAL==0:
+                tf_policy_saver.save( os.path.join(policy_dir,str(int(step/1e6))+'M_steps'))
+                train_checkpointer.save(global_step=agent.train_step_counter)
             print('--- Evaluation ---')
             eval_py_env.episode = 0
             average_return = compute_average_return(eval_tf_env, eval_policy, NUMBER_OF_EVAL_EPISODES, getcache=False)
@@ -192,7 +206,7 @@ def train_v1(init_policy=None):
             training_steps.append(step)
             phases.append(phase)
             times.append(time_elapse)
-            if np.mean(returns[-10:])>0.8 and phase<2:
+            if np.mean(returns[-10:])>0.8 and phase<3:
                 phase += 0
                 print('TRAINING IN PHASE {0}'.format(phase))
                 py_env.cache['phase']=phase
@@ -204,15 +218,14 @@ def train_v1(init_policy=None):
                     timeout=np.asarray(py_env.records['timeout']), outbound=np.asarray(py_env.records['outbound']))
 
 
-
     from matplotlib import pyplot as plt
 
     fig, ax = plt.subplots(2,1)
     ax[0].plot(training_steps,returns)
     ax[0].set_ylabel('average episode return')
     ax[0].set_xlabel('training steps')
-    ax[1].plot(losses)
-    ax[1].set_ylabel(np.linspace(np.min(training_steps), np.max(training_steps), len(losses) ),'loss')
+    ax[1].plot(np.linspace(np.min(training_steps), np.max(training_steps), len(losses) ), losses)
+    ax[1].set_ylabel('loss')
     ax[1].set_xlabel('training steps')
     plt.show()
 
