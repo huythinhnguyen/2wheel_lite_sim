e7eccfbd7a9c6c2d4f6165fc18f38b9fd2e991e6
thinh <huythinhnguyen@gmail.com>
2022-10-04 16:23:55 -0400

    .

--- Gym/BeaconDocking_v0/Helper.py
+++ Gym/BeaconDocking_v0/Helper.py
@@ -14,7 +14,7 @@ BEACON_MODE = 'bull'
 BEACON_HORN_ANGLE = np.pi/3
 
 def beacon2objects(x, y, theta, d, mode='bull', bull_angle=np.pi/3):
-    objects = np.asarray([x, y, 2], dtype=np.float32).reshape(1,3)
+    base = np.asarray([x, y, 2], dtype=np.float32).reshape(1,3)
     # make horn:
     if mode=='unicorn':
         horn = np.asarray([x + d*np.cos(theta), y +d*np.sin(theta), 1], dtype=np.float32).reshape(1,3)
@@ -22,4 +22,44 @@ def beacon2objects(x, y, theta, d, mode='bull', bull_angle=np.pi/3):
         horn = np.asarray([[x + d*np.cos(theta+bull_angle/2), y + d*np.sin(theta+bull_angle/2), 1],
                             [x + d*np.cos(theta-bull_angle/2), y + d*np.sin(theta-bull_angle/2), 1]], 
                             dtype=np.float32).reshape(2,3)
-    return np.vstack((objects, horn))
+    return np.vstack((base, horn))
+
+
+def beacons2objects(beacons: np.ndarray, d=0.3, mode='bull', bull_angle=np.pi/3):
+    objects = np.asarray([], dtype=np.float32).reshape(0,3)
+    for beacon in beacons:
+        x, y, theta = beacon
+        new = beacon2objects(x, y, theta, d=d, mode=mode, bull_angle=bull_angle)
+        objects = np.vstack((objects, new))
+    return objects
+
+
+def concatenate_beacons(beacon_objs: np.ndarray, objects: np.ndarray):
+    return np.vstack((beacon_objs, objects))
+
+
+def beacons_count(objects: np.ndarray):
+    ans = np.sum(objects[:,2])
+    if objects[1,2]!=objects[2,2]: return ans
+    else: return int(ans/2)
+
+
+def beacons_mode(objects: np.ndarray):
+    if (objects[1,2]!=objects[2,2]): return 'unicorn'
+    else: return 'bull'
+
+
+## Arena only consist of Plants if the number of beacons is not given
+def isolate_beacon_objs_from_objects(objects: np.ndarray, number_of_beacons=None):
+    if number_of_beacons is None:
+        number_of_beacons = beacons_count(objects)
+    mode = beacons_mode(objects)
+    beacon_objs = np.asarray([], dtype=np.float32).reshape(0,3)
+    t = 3 if mode=='bull' else 2
+    for i in range(number_of_beacons):
+        beacon_objs = np.vstack((beacon_objs, objects[t*i:t*i+t]))
+    return beacon_objs
+
+
+def objects2beacons(beacon_objs: np.ndarray, objects: np.ndarray):
+    return None
