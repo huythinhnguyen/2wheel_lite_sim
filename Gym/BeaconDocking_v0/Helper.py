bc9d467a9dd8234e3491575dbaca9cb01d047567
thinh <huythinhnguyen@gmail.com>
2022-10-05 13:09:48 -0400

    added the docking check functiongit add Gym/BeaconDocking_v0/Helper.py NOTE need to check on the docking angle. I am not sure

--- Gym/BeaconDocking_v0/Helper.py
+++ Gym/BeaconDocking_v0/Helper.py
@@ -9,11 +9,16 @@ if pathlib.Path(os.path.abspath(__file__)).parents[2] not in sys.path:
 from Sensors.BatEcho import Setting as sensorconfig
 from Arena import Builder
 
-BEACON_KLASS = 3
-BEACON_MODE = 'bull'
-BEACON_HORN_ANGLE = np.pi/3
 
-def beacon2objects(x, y, theta, d, mode='bull', bull_angle=np.pi/3):
+HIT_DISTANCE = {'pole': 0.3, 'plant': 0.4}
+OBJECT_SPACING = {'pole': 0.1, 'plant': 0.3}
+
+BEACON_SPECS = {'mode': 'bull', 'bull_angle': np.pi/3, 'hit_distance': 0.75,
+                'hit_angle': np.pi/2} 
+
+JITTER={'xy': 0.1, 'theta': np.pi/18}
+
+def beacon2objects(x, y, theta, d, mode=BEACON_SPECS['mode'], bull_angle=BEACON_SPECS['bull_angle']):
     base = np.asarray([x, y, 2], dtype=np.float32).reshape(1,3)
     # make horn:
     if mode=='unicorn':
@@ -25,7 +30,7 @@ def beacon2objects(x, y, theta, d, mode='bull', bull_angle=np.pi/3):
     return np.vstack((base, horn))
 
 
-def beacons2objects(beacons: np.ndarray, d=0.3, mode='bull', bull_angle=np.pi/3):
+def beacons2objects(beacons: np.ndarray, d=0.3, mode=BEACON_SPECS['mode'], bull_angle=BEACON_SPECS['bull_angle']):
     objects = np.asarray([], dtype=np.float32).reshape(0,3)
     for beacon in beacons:
         x, y, theta = beacon
@@ -58,7 +63,7 @@ def isolate_beacon_objs_from_objects(objects: np.ndarray, number_of_beacons=None
     beacon_objs = np.asarray([], dtype=np.float32).reshape(0,3)
     t = 3 if mode=='bull' else 2
     for i in range(number_of_beacons):
-        beacon_objs = np.vstack((beacon_objs, objects[t*i:t*i+t]))
+        beacon_objs = np.vstack((beacon_objs, objects[t*i:t(i+1)]))
     return beacon_objs
 
 
@@ -75,6 +80,39 @@ def object2beacon(beacon_obj: np.ndarray):
     return x, y, theta
 
 
-def objects2beacons(beacon_objs: np.ndarray, mode: str):
+def objects2beacons(beacon_objs: np.ndarray):
+    number_of_beacons = beacons_count(beacon_objs)
+    mode = beacons_mode(beacon_objs)
+    t = 3 if mode=='bull' else 2
+    beacons = np.zeros((number_of_beacons, 3))
+    for i in range(number_of_beacons):
+        beacons[i] = object2beacon(beacon_objs[t*i:t*(i+1)])
+    return beacons
+
+
+def docking_check_with_beacons(pose, beacons):
+    x, y, yaw = pose
+    cache={}
+    ego_beacons = np.copy(beacons)
+    ego_beacons[:,0] -= x
+    ego_beacons[:,1] -= y
     
-    return None
+    ego_beacons_polar = np.zeros((len(beacons),2))
+    ego_beacons_polar[:,0] = np.sqrt(np.power(ego_beacons[:,0],2) + np.power(ego_beacons[:,1],2))
+    ego_beacons_polar[:,1] = Builder.wrap2pi(np.arctan2((ego_beacons[:,1], ego_beacons[:,1])) - yaw)
+
+    ego_beacons = ego_beacons[ego_beacons_polar[:,0]<BEACON_SPECS['hit_distance']]
+    ego_beacons_polar = ego_beacons_polar[ego_beacons_polar[:,0]<BEACON_SPECS['hit_distance']]
+    ego_beacons = ego_beacons[np.abs(ego_beacons_polar[:,1])<sensorconfig.FOV_ANGULAR]
+    ego_beacons_polar = ego_beacons_polar[np.abs(ego_beacons_polar[:,1])<sensorconfig.FOV_ANGULAR]
+    
+    if len(ego_beacons_polar)==0: return False, cache
+    if (ego_beacons_polar > 1):
+        ego_beacons = ego_beacons[np.argsort( np.abs(Builder.wrap2pi(ego_beacons[:,2]-yaw)) )]
+
+    docking_angle = Builder.wrap2pi(Builder.wrap2pi(ego_beacons[0,2])-np.pi)
+    if (np.abs(docking_angle)>BEACON_SPECS['hit_angle']/2): return False, cache
+    cache['docking_angle'] = docking_angle
+    return True, cache
+
+
