3ecaa2779c66a017cf96a7ea1efe529c6f9cafbb
thinh <huythinhnguyen@gmail.com>
2022-10-31 16:30:22 -0400

    add file, incomplete

--- Gym/BeaconDocking_v0/notebooks/generate_dataset_docking_zone_classifier.py
+++ Gym/BeaconDocking_v0/notebooks/generate_dataset_docking_zone_classifier.py
@@ -3,4 +3,15 @@ import os
 import pathlib
 import sys
 
-if pathlib.Path(os.getcwd()).parent[2] not in sys.path
+if pathlib.Path(os.getcwd()).parents[2] not in sys.path:
+    sys.path.append(str(pathlib.Path(os.getcwd()).parents[2]))
+
+from Sensors.BatEcho.Spatializer import Render
+from Sensors.BatEcho import Setting as sensorconfig
+from Gym.BeaconDocking_v0 import Helper
+from Arena import Builder
+
+from Control.SensorimotorLoops.BatEcho import AvoidApproach, Avoid
+from Simulation.Motion import State
+from Control.SensorimotorLoops import Setting as controlconfig
+
